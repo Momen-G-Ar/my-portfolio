6b1404f5620746d569e42a143b798679c9f0eeda
Momen-G-Ar <cp.momen@gmail.com>
2023-08-03 20:59:50 +0300

    Edit the images location to public folder

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,4 @@
-import personalImage from '../assets/self-image.png';
+import personalImage from '../../public/self-image.png';
 
 const Home = () => {
     return (
@@ -11,16 +11,17 @@ const Home = () => {
                 `}
             >
                 <div id='up-left' className='flex flex-col justify-center items-center px-10'>
-                    <h1 className='ms:min-w-full flex flex-col justify-start items-start text-2xl gap-1'>
+                    <h1 className='ms:min-w-full flex flex-col justify-start items-start text-3xl gap-1'>
                         Hello,
-                        <span className='font-bold text-2xl md:text-5xl sm:text-3xl'> I'm Momin</span>
-                        <span className='font-bold text-lg  md:text-3xl sm:text-xl capitalize text-[var(--primary-color)]'> Full Stack web developer</span>
+                        <span className='font-bold text-2xl md:text-6xl sm:text-3xl'> I'm Momin</span>
+                        <span className='font-bold text-lg  md:text-5xl sm:text-xl capitalize text-[var(--primary-color)]'> Full Stack web developer</span>
                     </h1>
                 </div>
                 <div id='down-right' className='flex justify-center items-center'>
                     <div className='w-[70%] flex justify-center items-center'>
                         <img
                             src={personalImage}
+                            alt='Momin Personal Image'
                             className='sm:-translate-y-0 rounded-xl min-h-[230px] max-h-[420px] min-w-[230px] border-2 border-[var(--primary-color)] -translate-y-10'
                         />
                     </div>
